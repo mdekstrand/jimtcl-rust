0231dd89bfd72bb007954640aea1870fdc43aadc
Michael Ekstrand <md@ekstrandom.net>
2023-10-04 09:18:37 -0400

    successfully build w/ jim's autotools

--- jimtcl-sys/build.rs
+++ jimtcl-sys/build.rs
@@ -1,5 +1,17 @@
 use std::env;
-use std::path::PathBuf;
+use std::fs::create_dir_all;
+use std::path::{PathBuf, Path};
+use std::process::Command;
+use std::str::FromStr;
+
+fn run_cmd(stage: &str, mut cmd: Command) {
+  let mut proc = cmd.spawn().expect("error spawning process");
+  let res = proc.wait().expect("error waiting for process");
+  if !res.success() {
+    eprintln!("{}: failed with code {}", stage, res);
+    panic!("jim: build stage {} failed", stage);
+  }
+}
 
 fn create_bindings() {
   println!("cargo:rerun-if-changed=jim-wrapper.h");
@@ -20,28 +32,42 @@ fn create_bindings() {
        .expect("Couldn't write bindings!");
 }
 
-fn compile_jimtcl() {
-  let mut build = cc::Build::new();
-  build.include("jimtcl");
-  build.include(".");
-  build.define("HAVE_NO_AUTOCONF", None);
-  build.flag("-Wno-unused-parameter");
-  build.flag("-Wno-sign-compare");
-  // base Jim TCL source files
-  // build.file("_load-static-exts.c");
-  build.file("jimtcl/jim-subcmd.c");
-  build.file("jimtcl/jim-interactive.c");
-  build.file("jimtcl/jim-format.c");
-  build.file("jimtcl/jim.c");
-  build.file("jimtcl/utf8.c");
-  build.file("jimtcl/jimregexp.c");
-  build.file("jimtcl/jimiocompat.c");
+fn compile_jimtcl(src: &Path, build: &Path) {
+  let mut configure = src.canonicalize().expect("could not resolve source path");
+  configure.push("configure");
+  let target = env::var("TARGET").expect("no TARGET specified");
+  let host = env::var("HOST").expect("no HOST specified");
 
-  // compile JimTCL
-  build.compile("jim");
+  create_dir_all(build).expect("error creating build directory");
+
+  eprintln!("configuring jim");
+  eprintln!("path: {:?}", &configure);
+  eprintln!("build path: {:?}", build);
+  let mut cmd = Command::new("sh");
+  cmd.current_dir(build);
+  cmd.arg(configure);
+  if target != host {
+    cmd.arg(&format!("--host={}", target));
+  }
+  cmd.arg("--without-ext=default");
+  run_cmd("configure", cmd);
+
+  eprintln!("building jim");
+  let mut cmd = Command::new("make");
+  cmd.current_dir(build);
+  run_cmd("make", cmd);
+
+  println!("cargo:rustc-link-search={}", build.to_str().unwrap());
+  println!("cargo:rustc-link-lib=jim");
 }
 
 fn main() {
+  let build = env::var("OUT_DIR").expect("no OUT specified");
+  let build = PathBuf::from_str(&build).expect("invalid OUT path");
+
+  let src = PathBuf::from_str("jimtcl").expect("internal error");
+
+  println!("cargo:rerun-if-changed=build.rs");
+  compile_jimtcl(&src, &build);
   create_bindings();
-  compile_jimtcl();
 }
