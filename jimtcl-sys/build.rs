dedd1890d40a86c8efcd6ca62f5cbe125e06c97b
Michael Ekstrand <md@ekstrandom.net>
2023-10-04 09:26:44 -0400

    failed attempt at cross-compiling

--- jimtcl-sys/build.rs
+++ jimtcl-sys/build.rs
@@ -40,11 +40,23 @@ fn compile_jimtcl(src: &Path, build: &Path) {
 
   create_dir_all(build).expect("error creating build directory");
 
+  let mut ccb = cc::Build::new();
+  ccb.include("jimtcl");
+  ccb.include(build);
+
   eprintln!("configuring jim");
   eprintln!("path: {:?}", &configure);
   eprintln!("build path: {:?}", build);
   let mut cmd = Command::new("sh");
   cmd.current_dir(build);
+  let cc = ccb.get_compiler();
+  let ar = ccb.get_archiver();
+  let rl = ccb.get_ranlib();
+  cmd.env("CC", cc.cc_env());
+  cmd.env("CFLAGS", cc.cflags_env());
+  cmd.env("AR", ar.get_program());
+  cmd.env("RANLIB", rl.get_program());
+
   cmd.arg(configure);
   if target != host {
     cmd.arg(&format!("--host={}", target));
@@ -55,6 +67,7 @@ fn compile_jimtcl(src: &Path, build: &Path) {
   eprintln!("building jim");
   let mut cmd = Command::new("make");
   cmd.current_dir(build);
+  cmd.arg("libjim.a");
   run_cmd("make", cmd);
 
   println!("cargo:rustc-link-search={}", build.to_str().unwrap());
